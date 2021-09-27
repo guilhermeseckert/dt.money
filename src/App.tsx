ef4182017ab7ddea1c864fc9ee69d5511ed2b97e
Guilherme Eckert <guilherme-sonnemann@hotmail.com>
2021-09-27 16:09:55 -0700

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
-      <h2>Hello</h2>
+      <h1>Hello</h1>
+      <GlobalStyle />
     </div>
   );
 }
