c7c91b3c7e6179eee66841ff6fc0541c364cfed1
Guilherme Eckert <guilherme-sonnemann@hotmail.com>
2021-09-28 20:06:02 -0700

    added axios and miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
 import { App } from "./App";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "trnasaction 1",
+          amount: 1300,
+          type: "deposit",
+          category: "eletronics",
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
