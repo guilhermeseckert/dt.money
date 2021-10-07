e91becefe3fe18d4a7e6e3a0ce6703ea658a5f37
Guilherme Eckert <guilherme-sonnemann@hotmail.com>
2021-10-07 12:59:03 -0700

    update app Teste connect #2

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ import { GlobalStyle } from "./styles/global";
 import Modal from "react-modal";
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import {
-  TransactionContext,
-  TransactionProvider,
-} from "./TransasctionsContext";
+import { TransactionProvider } from "./TransasctionsContext";
 
 Modal.setAppElement("#root");
 
